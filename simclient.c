3770d539631ba0db2c17e354249a86f3ac34e45e
Justin King-Lacroix <justin.kinglacroix@gmail.com>
2013-08-18 17:13:10 +0100

    added simclient.c, which will form the basis of the sim backend library

---
+++ simclient.c
@@ -0,0 +1,54 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define DIE(x) { printf("Error %s.\n", x); exit(1); }
+
+int main(int argc, char* argv[]) {
+	int rport = 0;
+	if(!(argc != 1 && sscanf(argv[1], "%d", &rport) == 1))
+		DIE("in arguments");
+
+	printf("Remote port is %d\n", rport);
+
+	int sock = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+
+	if(sock == -1)
+		DIE("creating sock");
+
+	struct sockaddr_in remote_addr;
+
+	//memset((char*) &local_addr, 0, sizeof(local_addr));
+	//local_addr.sin_family = AF_INET;
+	//local_addr.sin_port = 0;
+	//local_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+
+	//if(bind(sock, (struct sockaddr*) &local_addr, sizeof(local_addr)) == -1)
+	//	DIE("binding sock");
+
+	//int slen = sizeof(local_addr);
+	//if(getsockname(sock, (struct sockaddr*) &local_addr, &slen) == -1)
+	//	DIE("getting socket info");
+
+	//printf("Socket bound to port %d\n", ntohs(local_addr.sin_port));
+
+	memset((char*) &remote_addr, 0, sizeof(remote_addr));
+	remote_addr.sin_family = AF_INET;
+	remote_addr.sin_port = htons(rport);
+	remote_addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
+
+	char buf[256];
+	memset(buf, 0, sizeof(buf));
+	if(sendto(sock, buf, 0, sizeof(buf), (struct sockaddr*) &remote_addr, sizeof(remote_addr)) == -1)
+		DIE("sending packet");
+
+	close(sock);
+	return 0;
+}
+
+
